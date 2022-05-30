3bbf783b57f52422aea6b1d555c01232982d639e
Kirill Rasuli <rasulikv@gmail.com>
2022-05-30 19:47:48 +0300

    Add flake8 and fix its issues

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -18,12 +18,14 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
 
     return response_object
 
+
 @router.get("/{id}/", response_model=SummarySchema)
 async def read_summary(id: int) -> SummarySchema:
     summary = await crud.get(id)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
     return summary
+
 
 @router.get("/", response_model=List[SummarySchema])
 async def read_all_summaries() -> List[SummarySchema]:
