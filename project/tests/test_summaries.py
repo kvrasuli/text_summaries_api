5797d155da477254f153006fced5ff0e66ce4a7c
Kirill Rasuli <rasulikv@gmail.com>
2022-05-30 21:15:05 +0300

    Update validation for post route

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -22,6 +22,9 @@ def test_create_summaries_invalid_json(test_app):
             }
         ]
     }
+    response = test_app.post("/summaries/", data=json.dumps({"url": "invalid://url"}))
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
 
 
 def test_read_summary(test_app_with_db):
